ae1d9079921d69ec3690200543a6fa3ab094eb07
Ardiyanto <ardiyantop8@gmail.com>
2023-01-16 13:57:16 +0700

    final UI form login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,26 +5,26 @@ export default function Login() {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
         <div className='hidden sm:block'>
-            <img src={loginImg} alt="" />
+            <img className='w-full h-full object-cover' src={loginImg} alt="" />
         </div>
 
-        <div>
-            <form>
-                <h2>SIGN IN</h2>
-                <div>
+        <div className='bg-gray-800 flex flex-col justify-center'>
+            <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+                <h2 className='text-4xl text-white font-bold text-center pb-4'>SIGN IN</h2>
+                <div className='flex flex-col text-gray-400 py-2'>
                     <label>User Name</label>
-                    <input type="text" />
+                    <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
                 </div>
 
-                <div>
+                <div className='flex flex-col text-gray-400 py-2'>
                     <label>Password</label>
-                    <input type="password" />
+                    <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" />
                 </div>
-                <div>
-                    <p><input type="checkbox" />Remember Me</p>
+                <div className='flex justify-between text-gray-400 py-2'>
+                    <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
                     <p>Forgot Password</p>
                 </div>
-                <button>Sign In</button>
+                <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 rounded-lg text-white font-semibold'>Sign In</button>
             </form>
         </div>
     </div>
